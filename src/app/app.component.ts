193984c932727b4321f54c718bec58616e0d915c
harrin33 <cycling.cobol@virgin.net>
2016-11-20 09:51:46 +0000

    changed to browser battle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Othello';
+  title = 'Browser battle';
   row = '<tr><td>another table</td></tr>';
   celltext = 'cell contents';
 }
